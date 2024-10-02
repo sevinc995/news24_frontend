27c70a8fe5772efc511718aed45a3bda54c47ce5
Nihad Nahmatli <bakueduaz@gmail.com>
2024-10-02 04:18:36 +0000

    changed

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
+$(document).ready(function () {
 
+    $(document).on('keypress', function (e) {
+        if (e.which = 13) {
+            let url = `https://apithree.vercel.app/api/news`;
+
+            async function mynews() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+
+                if (Array.isArray(data.news)) {
+                    data.news.forEach(item => {
+                        const box = document.createElement('div');
+                        box.className = 'col-md-4';
+                        box.innerHTML = `
+                            <div class="box">
+                                <div class="imgbox">
+                                    <img src="${item.img}" alt="">
+                                </div>
+                                <h1>${item.tit}</h1>
+                                <h4>${item.des}</h4>
+                                <div class="infobox">
+                                    <div class="lbtn">Good</div>
+                                    <div class="dbtn">Bad</div>
+                                </div>
+                            </div>
+                        `;
+                        $(".row").append(box);
+                    });
+                }
+            }
+
+            mynews();
+        }
+    });
+
+    $("h2").click(async function (e) {
+        e.preventDefault();
+        let url = `https://apithree.vercel.app/api/news`;
+
+        async function mynews() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            console.log(data);
+
+            if (Array.isArray(data.news)) {
+                data.news.forEach(item => {
+                    const box = document.createElement('div');
+                    box.className = 'col-md-4';
+                    box.innerHTML = `
+                            <div class="box">
+                                <div class="imgbox">
+                                    <img src="${item.img}" alt="">
+                                </div>
+                                <h1>${item.tit}</h1>
+                                <h4>${item.des}</h4>
+                                <div class="infobox">
+                                    <div class="lbtn">Good</div>
+                                    <div class="dbtn">Bad</div>
+                                </div>
+                            </div>
+                        `;
+                    $(".row").append(box);
+                });
+            }
+        }
+
+        mynews();
+    });
+});
